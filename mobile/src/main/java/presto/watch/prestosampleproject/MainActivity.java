832b2ee5fc16a7f08ae244f7d47b8938c77d21b4
David Jay <davidjay@samsungaccelerator.com>
2015-09-08 16:31:23 -0400

    Removing unecessary menu definition and adding comments to mobile MainActivity.

--- mobile/src/main/java/presto/watch/prestosampleproject/MainActivity.java
+++ mobile/src/main/java/presto/watch/prestosampleproject/MainActivity.java
@@ -1,6 +1,5 @@
 package presto.watch.prestosampleproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -16,6 +15,8 @@ public class MainActivity extends AbstractPhoneGestureActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//Override these functions to make your app respond to gestures.
 
     @Override
     public void onSnap() {
@@ -49,26 +50,5 @@ public class MainActivity extends AbstractPhoneGestureActivity {
     public void onWindowClosed() {
         Log.e("MainActivity","This function should not be called unless windowed gesture detection is enabled.");
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    
 }
